72cd61968dcd3c814b58063d50f04eb983476124
Atwinenickson <atwiinenicxon@gmail.com>
2022-08-11 00:52:30 +0300

    comment out code for localization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
-import LocalizationProvider from '@mui/x-date-pickers';
+import {LocalizationProvider} from '@mui/x-date-pickers';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
@@ -26,13 +26,13 @@ export default function MyApp(props: any) {
         </title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      {/* <LocalizationProvider dateAdapter={AdapterDateFns}> */}
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         {getLayout(<Component {...pageProps} />)}
       </ThemeProvider>
-      </LocalizationProvider>
+      {/* </LocalizationProvider> */}
     </CacheProvider>
   );
 }
