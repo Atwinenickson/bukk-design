0d375a2871839b5004d998c4108595a7b3584245
Atwinenickson <atwiinenicxon@gmail.com>
2022-08-10 23:38:18 +0300

    change folder locations for src and page and add card file

---
+++ src/pages/finance.tsx
@@ -0,0 +1,111 @@
+import Head from 'next/head';
+import { Box, Container, Grid } from '@mui/material';
+
+import { DashboardLayout } from '../components/dashboard-layout';
+
+const Finance = () => (
+  <>
+    <Head>
+      <title>
+        Dashboard | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <Sales />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <TrafficByDevice sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <LatestProducts sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+           <div>
+            Hello
+           </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
+Finance.getLayout = (page: any) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Finance;
