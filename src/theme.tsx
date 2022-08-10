7a2e659281648b69ad59a55114e2161fcd0e5149
Atwinenickson <atwiinenicxon@gmail.com>
2022-08-10 19:19:58 +0300

    fix tsx errors and all packages missing were added

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,17 +1,15 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+// src/theme.ts
 
-// Create a theme instance.
+import { createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: green.A700,
     },
     secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
+      main: '#ff6666',
     },
   },
 });
