a7ed635896ef18b66415695122a8659270b8c4e3
Atwinenickson <atwiinenicxon@gmail.com>
2022-08-14 12:58:36 +0300

    added the profitability graph details to every bar

--- src/graphs/profitability.js
+++ src/graphs/profitability.js
@@ -1,46 +1,57 @@
-import React from "react";
+import React, {useEffect} from "react";
+import Chart from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 import { Bar } from "react-chartjs-2";
 
 const ProfitabilityChart = () => {
+
+  useEffect(() => {
+    Chart.register(ChartDataLabels);
+})
+
   const profitabilityChartData = {
         labels:  ["BAKKIE", "TRUCK", "TRAILER"],
         datasets: [{
-          label: 'Vehicle Type',
+          label: 'Vehicle Types',
           data: [78, 95, 89],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
+            '#6484D6',
+            '#6484D6',
+            '#6484D6',
           ],
           borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
+            '#4A6BCC',
+            '#4A6BCC',
+            '#4A6BCC',
           ],
           borderWidth: 1
         }]
       }
 
+      const options = {
+        plugins: {
+          datalabels: {
+            display: true,
+            color: "white",
+            align: "end",
+            anchor: "end",
+            font: { size: "14" }
+          }
+        },
+        legend: {
+          display: false
+        }
+      };
+
   const barChart = (
     <Bar
       type="bar"
-    //   width={200}
-    //   height={500}
-      options={{
-        title: {
-          display: false,
-          text: "Profitability",
-          fontSize: 15
-        },
-        legend: {
-          display: false, //Is the legend shown?
-          position: "bottom" //Position of the legend.
-        }
-      }}
+      width={5} height={5}
+      options={options}
       data={profitabilityChartData}
     />
   );
   return barChart;
 };
 
-export default ProfitabilityChart;
+export default ProfitabilityChart
